000ba0cf70c61e076e098889ca2c78ad01557940
VaderSteve76 <alverson.s@yahoo.com>
2020-02-17 15:36:09 -0500

    Header & search components

--- movie-db/src/App.js
+++ movie-db/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+import Search from './Components/Search';
 
 function App() {
+  const apiurl = "http://www.omdbapi.com/?apikey=2e5834b0";
+
   return (
     <div className="App">
-
+      <header>
+        <h1>Movie DataBase</h1>
+      </header>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
