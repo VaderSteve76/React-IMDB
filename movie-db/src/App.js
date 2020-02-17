a74a075a62318cb88583c59a24426b03091f96db
VaderSteve76 <alverson.s@yahoo.com>
2020-02-17 16:01:40 -0500

    Search functionality working

--- movie-db/src/App.js
+++ movie-db/src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Search from './Components/Search';
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+
   const apiurl = "http://www.omdbapi.com/?apikey=2e5834b0";
+
+  const search = (e) => {
+    if(e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s)
+        .then(({ data }) => {
+          let results = data.search;
+          setState(prevState => {
+            return{ ...prevState, results: results }
+          })
+        });
+    }
+  };
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+    setState(prevState => {
+      return { ...prevState, s: s }
+    });
+  };
 
   return (
     <div className="App">
@@ -10,7 +36,7 @@ function App() {
         <h1>Movie DataBase</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} search={search} />
       </main>
     </div>
   );
