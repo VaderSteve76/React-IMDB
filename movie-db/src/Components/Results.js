2cc1cf74ee0b68afcbd780d96fe65214a13d12fa
VaderSteve76 <alverson.s@yahoo.com>
2020-02-18 22:34:01 -0500

    Final submission, everything working

--- movie-db/src/Components/Results.js
+++ movie-db/src/Components/Results.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Result from './Result';
 
-function Results({ results, opepPopup }) {
-  return (
-    <section className="results">
-      {results.map(result => (
-        <Result key={result.imdbID} result={result} openPopup={openPopup} />
-      ))}
-    </section>
-  )
+function Results ({ results, openPopup }) {
+	return (
+		<section className="results">
+			{results.map(result => (
+				<Result key={result.imdbID} result={result} openPopup={openPopup} />
+			))}
+		</section>
+	)
 }
 
-export default Results
+export default Results;
